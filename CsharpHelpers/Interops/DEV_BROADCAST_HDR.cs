1027f452090dce1cd3eff13d4c4ac57336ddd26c
ebmoluoc <47045513+ebmoluoc@users.noreply.github.com>
2019-06-26 23:39:25 -0400

    Device structures and constants

---
+++ CsharpHelpers/Interops/DEV_BROADCAST_HDR.cs
@@ -0,0 +1,12 @@
+﻿using System.Runtime.InteropServices;
+
+namespace CsharpHelpers.Interops
+{
+    [StructLayout(LayoutKind.Sequential)]
+    public struct DEV_BROADCAST_HDR
+    {
+        public int dbch_size;
+        public uint dbch_devicetype;
+        public uint dbch_reserved;
+    }
+}
--- CsharpHelpers/Interops/NativeConstants.cs
+++ CsharpHelpers/Interops/NativeConstants.cs
@@ -302,19 +302,6 @@ namespace CsharpHelpers.Interops
         public const int HTCLOSE = 20;
         public const int HTHELP = 21;
 
-        public const int DBT_DEVICEQUERYREMOVE = 0x8001;
-        public const int DBT_DEVICEQUERYREMOVEFAILED = 0x8002;
-        public const int DBT_DEVICEREMOVEPENDING = 0x8003;
-        public const int DBT_DEVICEREMOVECOMPLETE = 0x8004;
-        public const int DBT_DEVICETYPESPECIFIC = 0x8005;
-        public const int DBT_CUSTOMEVENT = 0x8006;
-        public const int DBT_DEVNODES_CHANGED = 0x0007;
-        public const int DBT_QUERYCHANGECONFIG = 0x0017;
-        public const int DBT_CONFIGCHANGED = 0x0018;
-        public const int DBT_CONFIGCHANGECANCELED = 0x0019;
-        public const int DBT_DEVICEARRIVAL = 0x8000;
-        public const int DBT_USERDEFINED = 0xFFFF;
-
         public const int SW_HIDE = 0;
         public const int SW_SHOWNORMAL = 1;
         public const int SW_NORMAL = 1;
@@ -391,6 +378,30 @@ namespace CsharpHelpers.Interops
         public const int GCLP_HMODULE = -16;
         public const int GCLP_WNDPROC = -24;
         public const int GCLP_HICONSM = -34;
+
+        public const int DBT_DEVICEQUERYREMOVE = 0x8001;
+        public const int DBT_DEVICEQUERYREMOVEFAILED = 0x8002;
+        public const int DBT_DEVICEREMOVEPENDING = 0x8003;
+        public const int DBT_DEVICEREMOVECOMPLETE = 0x8004;
+        public const int DBT_DEVICETYPESPECIFIC = 0x8005;
+        public const int DBT_CUSTOMEVENT = 0x8006;
+        public const int DBT_DEVNODES_CHANGED = 0x0007;
+        public const int DBT_QUERYCHANGECONFIG = 0x0017;
+        public const int DBT_CONFIGCHANGED = 0x0018;
+        public const int DBT_CONFIGCHANGECANCELED = 0x0019;
+        public const int DBT_DEVICEARRIVAL = 0x8000;
+        public const int DBT_USERDEFINED = 0xFFFF;
+
+        public const ushort DBTF_MEDIA = 0x0001;
+        public const ushort DBTF_NET = 0x0002;
+
+        public const uint DBT_DEVTYP_OEM = 0x00000000;
+        public const uint DBT_DEVTYP_DEVNODE = 0x00000001;
+        public const uint DBT_DEVTYP_VOLUME = 0x00000002;
+        public const uint DBT_DEVTYP_PORT = 0x00000003;
+        public const uint DBT_DEVTYP_NET = 0x00000004;
+        public const uint DBT_DEVTYP_DEVICEINTERFACE = 0x00000005;
+        public const uint DBT_DEVTYP_HANDLE = 0x00000006;
 
         public const uint WS_OVERLAPPED = 0x00000000;
         public const uint WS_POPUP = 0x80000000;
